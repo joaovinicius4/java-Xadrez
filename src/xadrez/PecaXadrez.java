1f41b96dfba38e6659712006d030c491da87fac4
joaovinicius4 <joaoruas111@gmail.com>
2026-03-05 23:26:48 -0300

    refactor: change piece constructor to receive board instead of position

--- src/xadrez/PecaXadrez.java
+++ src/xadrez/PecaXadrez.java
@@ -2,13 +2,14 @@ package xadrez;
 
 import jogo_tabuleiro.Peca;
 import jogo_tabuleiro.Posicao;
+import jogo_tabuleiro.Tabuleiro;
 
 public class PecaXadrez  extends Peca{
 
 	private Cor cor;
 
-	public PecaXadrez(Posicao posicao, Cor cor) {
-		super(posicao);
+	public PecaXadrez(Tabuleiro tabuleiro, Cor cor) {
+		super(tabuleiro);
 		this.cor = cor;
 	}
 
