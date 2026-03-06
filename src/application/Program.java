8e1e3f33f352f0b8b93286565d1ee369ffbf3c80
joaovinicius4 <joaoruas111@gmail.com>
2026-03-05 21:55:49 -0300

    Create chess match and print board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import xadrez.PartidaXadrez;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		PartidaXadrez partidaXadrez = new PartidaXadrez();
+		Imprimir.imprimirTabuleiro(partidaXadrez.getPecas());
 
 	}
 
