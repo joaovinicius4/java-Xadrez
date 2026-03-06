7763e5e89144645588da0ab136f019b727f62213
joaovinicius4 <joaoruas111@gmail.com>
2026-03-05 21:01:20 -0300

    refactor: add board attribute and getter to piece

--- src/jogo_tabuleiro/Peca.java
+++ src/jogo_tabuleiro/Peca.java
@@ -3,5 +3,15 @@ package jogo_tabuleiro;
 public class Peca {
 
 	protected Posicao posicao;
+	private Tabuleiro tabuleiro;
+	
+	public Peca(Posicao posicao) {
+		this.posicao = posicao;
+	}
 
+	protected Tabuleiro getTabuleiro() {
+		return tabuleiro;
+	}
+	
+	
 }
