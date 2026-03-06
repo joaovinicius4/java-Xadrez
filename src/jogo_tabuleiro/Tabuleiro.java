b1dfc08e9d288c5c72d341dcfea8415a534ef894
joaovinicius4 <joaoruas111@gmail.com>
2026-03-06 08:56:39 -0300

    Add position validation and piece placement checks in Tabuleiro

--- src/jogo_tabuleiro/Tabuleiro.java
+++ src/jogo_tabuleiro/Tabuleiro.java
@@ -24,14 +24,23 @@ public class Tabuleiro {
 	}
 	
 	public Peca peca(int linha, int coluna) {
+		if(!posicaoExiste(linha, coluna)) {
+			throw new ExcecaoTabuleiro("Nao tem essa posição no tabuleiro");
+		}
 		return pecas[linha][coluna];
 	}
 	
 	public Peca peca(Posicao posicao) {
+		if(!posicaoExiste(posicao)) {
+			throw new ExcecaoTabuleiro("Nao tem essa posição no tabuleiro");
+		}
 		return pecas[posicao.getLinha()][posicao.getColuna()];
 	}
 	
 	public void lugarPeca(Peca peca, Posicao posicao) {
+		if(temPeca(posicao)) {
+			throw new ExcecaoTabuleiro("Tem peça nesse lugar " + posicao);
+		}
 		pecas[posicao.getLinha()][posicao.getColuna()] = peca;
 		peca.posicao = posicao; // como coloquei protected eu consigo acessar dentro do mesmo pacote;
 	}
@@ -45,6 +54,9 @@ public class Tabuleiro {
 	}
 	
 	public boolean temPeca(Posicao posicao) {
+		if(!posicaoExiste(posicao)) {
+			throw new ExcecaoTabuleiro("Nao tem essa posição no tabuleiro");
+		}
 		return peca(posicao) != null;
 	}
 	
